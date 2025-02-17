e50cefbab5c3df1665f5503b61418c7d890988cc
k-preslav <96019707+k-preslav@users.noreply.github.com>
2025-02-17 14:13:15 +0200

    working

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 import './App.css';
 import ClassroomsPanel from './components/ClassroomsPanel/ClassroomsPanel';
 import NavMenu from './components/NavMenu/NavMenu';
 import ProfilePanel from './components/ProfilePanel/ProfilePanel';
 import Assignments from './components/Assignments/Assignments';
 import AssignmentDetails from './components/Assignments/AssignmentDetails';
+import Grades from './components/Grades/Grades';
+import SettingsPanel from './components/Settings/SettingsPanel';
+import Settings from './components/Settings/Settings';
 
 function App() {
   const [activeTab, setActiveTab] = useState('tasks');
   const [selectedAssignment, setSelectedAssignment] = useState(null);
   const [selectedClassroom, setSelectedClassroom] = useState(null);
   const [isMobileContentVisible, setIsMobileContentVisible] = useState(false);
+  const [selectedSetting, setSelectedSetting] = useState(null);
 
   const handleClassroomSelect = (classroom) => {
     setSelectedClassroom(classroom);
@@ -25,6 +29,13 @@ function App() {
     }
   };
 
+  const setSelectedAssignmentCallback = useCallback(
+    (assignment) => {
+      setSelectedAssignment(assignment);
+    },
+    [setSelectedAssignment]
+  );
+
   const renderSidebarContent = () => {
     switch (activeTab) {
       case 'tasks':
@@ -32,14 +43,18 @@ function App() {
       case 'grades':
         return <ClassroomsPanel setSelectedClassroom={handleClassroomSelect} />;
       case 'settings':
-        return <p>Settings Panel</p>;
+        return <SettingsPanel setSelectedSetting={setSelectedSetting} />;
       default:
         return <p>Моля селектирайте поле</p>;
     }
   };
 
   return (
-    <div className={`dashboard ${isMobileContentVisible ? 'mobile-content-visible' : ''} ${selectedAssignment ? 'assignment-selected' : ''}`}>
+    <div
+      className={`dashboard ${
+        isMobileContentVisible ? 'mobile-content-visible' : ''
+      } ${selectedAssignment ? 'assignment-selected' : ''}`}
+    >
       <aside className="sidebar">
         <div>
           <ProfilePanel
@@ -50,30 +65,52 @@ function App() {
 
           <NavMenu activeButton={activeTab} setActiveButton={setActiveTab} />
 
-          <div id='sidebar-content'>{renderSidebarContent()}</div>
+          <div id="sidebar-content">{renderSidebarContent()}</div>
         </div>
       </aside>
 
       <main className="content">
-        <section className="content-left panel">
+        <section
+          className="content-left panel"
+          style={{ display: activeTab === 'tasks' ? 'block' : 'none' }}
+        >
           {isMobileContentVisible && !selectedAssignment && (
             <button className="back-button" onClick={handleBackClick}>
               ← Назад към класни стаи
             </button>
           )}
-          <Assignments 
-            selectedClassroom={selectedClassroom || "Няма избрана класна стая"}
-            setSelectedAssignment={setSelectedAssignment}
-          />
+          {activeTab === 'tasks' && (
+            <Assignments
+              selectedClassroom={
+                selectedClassroom || 'Няма избрана класна стая'
+              }
+              setSelectedAssignment={setSelectedAssignmentCallback}
+            />
+          )}
         </section>
 
-        <section className="content-right panel">
-          {selectedAssignment && (
-            <button className="back-button" onClick={handleBackClick}>
-              ← Назад към задания
-            </button>
+        <section
+          className="content-right panel"
+          style={{
+            gridColumn: activeTab !== 'tasks' ? '1 / span 2' : '2',
+          }}
+        >
+          {activeTab === 'tasks' && (
+            <>
+              {selectedAssignment && (
+                <button className="back-button" onClick={handleBackClick}>
+                  ← Назад към задания
+                </button>
+              )}
+              <AssignmentDetails assignment={selectedAssignment} />
+            </>
           )}
-          <AssignmentDetails assignment={selectedAssignment} />
+          {activeTab === 'grades' && (
+            <Grades selectedClassroom={selectedClassroom} />
+          )}
+          {activeTab === 'settings' && (
+            <Settings selectedSetting={selectedSetting} />
+          )}
         </section>
       </main>
     </div>
--- src/components/Assignments/Assignments.jsx
+++ src/components/Assignments/Assignments.jsx
@@ -1,17 +1,24 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useMemo } from 'react';
 import Button from '../Button/Button';
 import './Assignments.css';
-import { calcTruncateWinWidth, truncateString } from '../../backend/truncateString';
+import {
+  calcTruncateWinWidth,
+  truncateString,
+} from '../../backend/truncateString';
 
 function Assignments({ selectedClassroom, setSelectedAssignment }) {
   const [activeAssignment, setActiveAssignment] = useState(null);
   const [truncatedAssignments, setTruncatedAssignments] = useState([]);
-  const assignments = selectedClassroom?.assignments || [];
+
+  // Memoize the assignments array
+  const assignments = useMemo(() => {
+    return selectedClassroom?.assignments || [];
+  }, [selectedClassroom?.assignments]);
 
   useEffect(() => {
     // Update truncated assignments when assignments change
     updateTruncateAssignments(assignments);
-    
+
     // Reset active and selected assignments
     setActiveAssignment(null);
     setSelectedAssignment(null);
@@ -30,9 +37,9 @@ function Assignments({ selectedClassroom, setSelectedAssignment }) {
   }, [assignments]);
 
   const updateTruncateAssignments = (assignments) => {
-    const truncateLength = window.innerWidth <= 768 ? "9cnt" : "2.2cnt";
-    
-    const updatedAssignments = assignments.map(ass => ({
+    const truncateLength = window.innerWidth <= 768 ? '9cnt' : '2.2cnt';
+
+    const updatedAssignments = assignments.map((ass) => ({
       ...ass,
       truncTitle: truncateString(ass.title, truncateLength),
     }));
@@ -41,14 +48,18 @@ function Assignments({ selectedClassroom, setSelectedAssignment }) {
 
   const handleClick = (id) => {
     setActiveAssignment(id);
-    setSelectedAssignment(truncatedAssignments.find(assignment => assignment.id === id));
+    setSelectedAssignment(
+      truncatedAssignments.find((assignment) => assignment.id === id)
+    );
   };
 
   return (
     <div id="assignments">
       <div id="assignments-header">
         <h1>Задания</h1>
-        <p className='smallText'>{selectedClassroom.title || "Няма избрана класна стая"}</p>
+        <p className="smallText">
+          {selectedClassroom.title || 'Няма избрана класна стая'}
+        </p>
       </div>
 
       <br />
@@ -56,11 +67,11 @@ function Assignments({ selectedClassroom, setSelectedAssignment }) {
       <div id="not-submitted">
         <h2>Непредадени</h2>
 
-        <div className='horizontal-layout'>
+        <div className="horizontal-layout">
           <div className="vertical-line"></div>
-          <div className='assignments-list'>
+          <div className="assignments-list">
             {truncatedAssignments.map((assignment) => (
-              <Button 
+              <Button
                 key={assignment.id}
                 content={assignment.truncTitle}
                 active={activeAssignment === assignment.id}
